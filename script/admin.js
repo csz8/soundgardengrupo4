6782bee1c3d77a36fce64cc0d76a1666e303d3ea
Célio Soares <cszenc@gmail.com>
2022-03-30 06:29:55 -0300

    criando arquivos js da exclusao de evento

---
+++ script/admin.js
@@ -0,0 +1,12 @@
+// Excluir Evento
+
+const btnExcluir = document.querySelector(".btn.btn-danger");
+console.log(btnExcluir);
+
+btnExcluir.onclick = (evento) => {
+  evento.preventDefault();
+
+  const inputData = document.querySelector("#data");
+  const inputNome = document.querySelector("#nome");
+  const inputAtracoes = document.querySelector("#atracoes");
+};
---
+++ script/excluir-evento.js
@@ -0,0 +1,12 @@
+// Listar Eventos
+
+const inputNome = document.querySelector("#nome");
+const inputBanner = document.querySelector("#banner");
+const inputAtracoes = document.querySelector("#atracoes");
+const inputDescricao = document.querySelector("#descricao");
+const inputData = document.querySelector("#data");
+const inputLotacao = document.querySelector("#lotacao");
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com/events'";
+
+
+window.location.origin === "/admin.html"
