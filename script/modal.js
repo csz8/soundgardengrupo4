101dd06b7616791e0eb87de6bc8353825ef4a060
Célio Soares <cszenc@gmail.com>
2022-03-31 00:49:12 -0300

    adicionando teste de modal

---
+++ script/modal.js
@@ -0,0 +1,22 @@
+const sectionIndex = document.querySelector(".container.text-center");
+console.log(sectionIndex);
+
+sectionIndex.innerHTML = `
+<div class="container text-center ">
+<h2>Próximos eventos</h2>
+</div>
+
+<div class="modal">
+
+<header>
+<h2>Reserva de Ingresso</h2>
+<span class="fechar-modal">X</span>
+</header>
+
+<div class="corpo-modal">
+<form>
+
+
+</div>
+
+`;
