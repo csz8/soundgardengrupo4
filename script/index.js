8c7a3ac81b3930948aaaaefd1a96a545ef35e29d
Célio Soares <cszenc@gmail.com>
2022-04-01 06:53:35 -0300

    criada atualização de evento na home

--- script/index.js
+++ script/index.js
@@ -1,24 +1,88 @@
-//Incluir html com o código do modal
+//Listar eventos com ID
 
-const sectionIndex = document.querySelector("main");
-console.log(sectionIndex);
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+const eventosLista = document.querySelector(".container.d-flex.justify-content-center.align-items-center.events");
+const metodo = {
+  method: "GET",
+  redirect: "follow",
+};
 
-atualizaIndex => {
-    sectionIndex.innerHTML += `<div class="modal">
-<div class="modal-topo">
-    <h2>Reserva de Ingresso</h2>
-    <span class="fechar-modal">x</span>
-</div>
-<div class="modal-corpo">
-    <form action="">
-        <label for="name">Nome Completo</label><br>
-        <input type="text" id="nome-modal" name="nome-modal" placeholder="digite seu nome aqui"><br>
-        <label for="mail">E-mail</label><br>
-        <input type="email" id="email-modal" name="email-modal" placeholder="digite seu e-mail aqui"><br>
-        <input type="submit" id="confirmar-reserva" value="Reservar Agora">
-    </form>
-    <p>Ao clicar no botão você confirmará sua reserva para o evento.</p>
-</div>
-</div>`;
+const listarEventos = async () => {
+  const resposta = await fetch(`${BASE_URL}/events`, metodo);
+  const conteudoResposta = await resposta.json();
+  let novaLista = "";
+
+  for (let index = 0; index < 3; index++) {
+    novaLista = `<article class="evento card p-5 m-3"><h2>${conteudoResposta[index].name} - ${conteudoResposta[index].scheduled}</h2>
+    <h4>${conteudoResposta[index].attractions}</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aperiam sunt quo similique,
+        dolorum consectetur inventore ipsam, officiis neque natus eius harum alias quidem. Possimus
+        nobis in inventore tenetur asperiores.</p>
+        <p>IdEvento: ${conteudoResposta[index]._id}</p>
+    <a href="#" class="btn btn-primary">reservar ingresso</a>`;
+  }
+  eventosLista.innerHTML = novaLista;
+
+  for (let index = 0; index < 2; index++) {
+    novaLista = `<article class="evento card p-5 m-3"><h2>${conteudoResposta[index].name} - ${conteudoResposta[index].scheduled}</h2>
+    <h4>${conteudoResposta[index].attractions}</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aperiam sunt quo similique,
+        dolorum consectetur inventore ipsam, officiis neque natus eius harum alias quidem. Possimus
+        nobis in inventore tenetur asperiores.</p>
+        <p>IdEvento: ${conteudoResposta[index]._id}</p>
+    <a href="#" class="btn btn-primary">reservar ingresso</a>`;
+  }
+  eventosLista.innerHTML += novaLista;
+
+  for (let index = 0; index < 1; index++) {
+    novaLista = `<article class="evento card p-5 m-3"><h2>${conteudoResposta[index].name} - ${conteudoResposta[index].scheduled}</h2>
+    <h4>${conteudoResposta[index].attractions}</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aperiam sunt quo similique,
+        dolorum consectetur inventore ipsam, officiis neque natus eius harum alias quidem. Possimus
+        nobis in inventore tenetur asperiores.</p>
+        <p>IdEvento: ${conteudoResposta[index]._id}</p>
+    <a href="#" class="btn btn-primary">reservar ingresso</a>`;
+  }
+  eventosLista.innerHTML += novaLista;
+
+};
+
+listarEventos();
+
+//Ativar modal
+
+const eventos = document.querySelectorAll("btn.btn-primary");
+
+const modal = document.querySelector(".modal");
+console.log(eventos);
+
+for (let i = 0; i < eventos.length; i++) {
+  eventos[i].addEventListener("click", (evento) => {
+    evento.preventDefault();
+  });
 }
-console.log(sectionIndex);
+
+for (let i = 0; i < eventos.length; i++) {
+  eventos[i].addEventListener("click", () => {
+    modal.classList.add("-ativar");
+  });
+}
+
+const btnModal = document.querySelectorAll(".btn-modal");
+console.log(btnModal);
+
+for (let i = 0; i < btnModal.length; i++) {
+  btnModal[i].addEventListener("click", (evento) => {
+    evento.preventDefault();
+  });
+}
+
+for (let i = 0; i < btnModal.length; i++) {
+  btnModal[i].addEventListener("click", () => {
+    modal.classList.remove("-ativar");
+  });
+}
+
+
+//Gerar reserva
+
