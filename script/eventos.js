d2dcce7d716c1e53ccb9539217d6200643bd73d6
Célio Soares <cszenc@gmail.com>
2022-04-01 07:11:27 -0300

    atualizados os js de eventos e index

---
+++ script/eventos.js
@@ -0,0 +1,62 @@
+//Listar eventos com ID
+
+const BASE_URL = "https://xp41-soundgarden-api.herokuapp.com";
+const eventosLista = document.querySelector(".container.d-flex.justify-content-center.align-items-center.flex-wrap");
+console.log(eventosLista);
+const metodo = {
+  method: "GET",
+  redirect: "follow",
+};
+
+const listarEventos = async () => {
+  const resposta = await fetch(`${BASE_URL}/events`, metodo);
+  const conteudoResposta = await resposta.json();
+  let novaLista = "";
+
+  for (let index = 0; index < conteudoResposta.length; index++) {
+    novaLista += `<div><article class="evento card p-5 m-3"><h2>${conteudoResposta[index].name} - ${conteudoResposta[index].scheduled}</h2>
+    <h4>${conteudoResposta[index].attractions}</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aperiam sunt quo similique,
+        dolorum consectetur inventore ipsam, officiis neque natus eius harum alias quidem. Possimus
+        nobis in inventore tenetur asperiores.</p>
+        <p>IdEvento: ${conteudoResposta[index]._id}</p>
+    <a href="#" class="btn btn-primary">reservar ingresso</a></div>`;
+  }
+  eventosLista.innerHTML = novaLista;
+};
+
+listarEventos();
+
+//Ativar modal
+
+const eventos = document.querySelectorAll("btn.btn-primary");
+
+const modal = document.querySelector(".modal");
+console.log(eventos);
+
+for (let i = 0; i < eventos.length; i++) {
+  eventos[i].addEventListener("click", (evento) => {
+    evento.preventDefault();
+  });
+}
+
+for (let i = 0; i < eventos.length; i++) {
+  eventos[i].addEventListener("click", () => {
+    modal.classList.add("-ativar");
+  });
+}
+
+const btnModal = document.querySelectorAll(".btn-modal");
+console.log(btnModal);
+
+for (let i = 0; i < btnModal.length; i++) {
+  btnModal[i].addEventListener("click", (evento) => {
+    evento.preventDefault();
+  });
+}
+
+for (let i = 0; i < btnModal.length; i++) {
+  btnModal[i].addEventListener("click", () => {
+    modal.classList.remove("-ativar");
+  });
+}
